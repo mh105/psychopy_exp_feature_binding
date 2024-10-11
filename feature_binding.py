835f2c438b841079ba8fbbaac66d3bceff5cbf5d
mh105 <alexhe0218@gmail.com>
2024-10-10 19:42:35 -0700

    BF: remove unused routine

--- feature_binding.py
+++ feature_binding.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2024.2.2a1),
-    on Thu Oct 10 16:21:59 2024
+    on Thu Oct 10 19:41:45 2024
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
